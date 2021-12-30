3d7c4bd901765939bba9458e89ed8d836ab9468b
PlasmaBase <78370529+SabberFlasche@users.noreply.github.com>
2021-12-30 11:14:31 +0100

    Globalized annotation repository

--- src/main/java/me/plasmabase/pastimegames/helper/eventsystem/GameResultConnect4.java
+++ src/main/java/me/plasmabase/pastimegames/helper/eventsystem/GameResultConnect4.java
@@ -2,8 +2,7 @@ package me.plasmabase.pastimegames.helper.eventsystem;
 
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class GameResultConnect4 extends GameResult{
 
