267d77a62ee98fc75d6aff50943651a2e4c70275
PlasmaBase <78370529+SabberFlasche@users.noreply.github.com>
2022-01-02 12:43:09 +0100

    modified settings for implementation of TicTacToe

--- src/main/java/me/plasmabase/pastimegames/manager/SettingsManager.java
+++ src/main/java/me/plasmabase/pastimegames/manager/SettingsManager.java
@@ -1,6 +1,6 @@
 package me.plasmabase.pastimegames.manager;
 
-import me.plasmabase.pastimegames.Main;
+import me.plasmabase.pastimegames.PastimeGames;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
@@ -10,30 +10,32 @@ public class SettingsManager {
     private final FileConfiguration config;
     private final String version;
 
+    private final String path_version = "version";
     private final String path_reloadMessage = "reload-message";
     private final String path_commands_noPermission = "commands.noPermission";
     private final String path_commands_wrongSyntax = "commands.wrongSyntax";
     private final String path_connect4_invTitle = "connect4.invTitle";
-    private final String path_connect4_items_placeholder_displayName = "connect4.items.placeholder.displayName";
-    private final String path_connect4_items_yourTurn_displayName = "connect4.items.yourTurn.displayName";
-    private final String path_connect4_items_opponentsTurn_displayName = "connect4.items.opponentsTurn.displayName";
-    private final String path_connect4_items_yourChipItem_displayName = "connect4.items.yourChipItem.displayName";
-    private final String path_connect4_items_opponentsChipItem_displayName =
-            "connect4.items.opponentsChipItem.displayName";
+    private final String path_tictactoe_invTitle = "tictactoe.invTitle";
+    private final String path_items_placeholder_displayName = "items.placeholder.displayName";
+    private final String path_items_yourTurn_displayName = "items.yourTurn.displayName";
+    private final String path_items_opponentsTurn_displayName = "items.opponentsTurn.displayName";
+    private final String path_items_yourChipItem_displayName = "items.yourChipItem.displayName";
+    private final String path_items_opponentsChipItem_displayName = "items.opponentsChipItem.displayName";
 
     private String reloadMessage;
     private String commands_noPermission;
     private String commands_wrongSyntax;
     private String connect4_invTitle;
-    private String connect4_items_placeholder_displayName;
-    private String connect4_items_yourTurn_displayName;
-    private String connect4_items_opponentsTurn_displayName;
-    private String connect4_items_yourChipItem_displayName;
-    private String connect4_items_opponentsChipItem_displayName;
+    private String tictactoe_invTitle;
+    private String items_placeholder_displayName;
+    private String items_yourTurn_displayName;
+    private String items_opponentsTurn_displayName;
+    private String items_yourChipItem_displayName;
+    private String items_opponentsChipItem_displayName;
 
     public SettingsManager() {
-        this.config = Main.plugin().getConfig();
-        this.version = Main.plugin().getDescription().getVersion();
+        this.config = PastimeGames.plugin().getConfig();
+        this.version = PastimeGames.plugin().getDescription().getVersion();
         this.loadConfig();
         this.checkVersion();
     }
@@ -43,21 +45,22 @@ public class SettingsManager {
         this.commands_noPermission = this.config.getString(path_commands_noPermission);
         this.commands_wrongSyntax = this.config.getString(path_commands_wrongSyntax);
         this.connect4_invTitle = this.config.getString(path_connect4_invTitle);
-        this.connect4_items_placeholder_displayName = this.config.getString(path_connect4_items_placeholder_displayName);
-        this.connect4_items_yourTurn_displayName = this.config.getString(path_connect4_items_yourTurn_displayName);
-        this.connect4_items_opponentsTurn_displayName = this.config.getString(path_connect4_items_opponentsTurn_displayName);
-        this.connect4_items_yourChipItem_displayName = this.config.getString(path_connect4_items_yourChipItem_displayName);
-        this.connect4_items_opponentsChipItem_displayName = this.config.getString(path_connect4_items_opponentsChipItem_displayName);
+        this.tictactoe_invTitle = this.config.getString(path_tictactoe_invTitle);
+        this.items_placeholder_displayName = this.config.getString(path_items_placeholder_displayName);
+        this.items_yourTurn_displayName = this.config.getString(path_items_yourTurn_displayName);
+        this.items_opponentsTurn_displayName = this.config.getString(path_items_opponentsTurn_displayName);
+        this.items_yourChipItem_displayName = this.config.getString(path_items_yourChipItem_displayName);
+        this.items_opponentsChipItem_displayName = this.config.getString(path_items_opponentsChipItem_displayName);
     }
 
     /**
      * Reloads the config
      */
     public void reloadConfig() {
-        Main.plugin().reloadConfig();
+        PastimeGames.plugin().reloadConfig();
         this.loadConfig();
         this.checkVersion();
-        Main.plugin().getLogger().info("config.yml reloaded");
+        PastimeGames.plugin().getLogger().info("config.yml reloaded");
     }
 
     private void checkVersion() {
@@ -71,16 +74,17 @@ public class SettingsManager {
     }
 
     private void fixConfig() {
-        config.set("version", version);
-        config.set("reload-message", reloadMessage);
-        config.set("commands.noPermission", commands_noPermission);
-        config.set("commands.wrongSyntax", commands_wrongSyntax);
-        config.set("connect4.invTitle", connect4_invTitle);
-        config.set("connect4.items.placeholder.displayName", connect4_items_placeholder_displayName);
-        config.set("connect4.items.yourTurn.displayName", connect4_items_yourTurn_displayName);
-        config.set("connect4.items.opponentsTurn.displayName", connect4_items_opponentsTurn_displayName);
-        config.set("connect4.items.yourChipItem.displayName", connect4_items_yourChipItem_displayName);
-        config.set("connect4.items.opponentsChipItem.displayName", connect4_items_opponentsChipItem_displayName);
+        config.set(path_version, version);
+        config.set(path_reloadMessage, reloadMessage);
+        config.set(path_commands_noPermission, commands_noPermission);
+        config.set(path_commands_wrongSyntax, commands_wrongSyntax);
+        config.set(path_connect4_invTitle, connect4_invTitle);
+        config.set(path_tictactoe_invTitle, tictactoe_invTitle);
+        config.set(path_items_placeholder_displayName, items_placeholder_displayName);
+        config.set(path_items_yourTurn_displayName, items_yourTurn_displayName);
+        config.set(path_items_opponentsTurn_displayName, items_opponentsTurn_displayName);
+        config.set(path_items_yourChipItem_displayName, items_yourChipItem_displayName);
+        config.set(path_items_opponentsChipItem_displayName, items_opponentsChipItem_displayName);
         this.save();
     }
 
@@ -88,7 +92,7 @@ public class SettingsManager {
      * Saves the config
      */
     public void save() {
-        Main.plugin().saveConfig();
+        PastimeGames.plugin().saveConfig();
     }
 
     /**
@@ -164,10 +168,28 @@ public class SettingsManager {
     }
 
     /**
+     * @return tictactoe_invTitle
+     */
+    public @NotNull String tictactoeInventoryTitle() {;
+        return ChatColor.translateAlternateColorCodes('&', tictactoe_invTitle);
+    }
+
+    /**
+     * Sets parameter and then saves it to the config.
+     * ColorChat: '&'
+     * @param tictactoe_invTitle parameter to set
+     */
+    public void tictactoeInventoryTitle(@NotNull String tictactoe_invTitle) {
+        this.tictactoe_invTitle = tictactoe_invTitle;
+        this.config.set(path_tictactoe_invTitle, tictactoe_invTitle);
+        this.save();
+    }
+
+    /**
      * @return connect4_items_placeholder_displayName
      */
-    public @NotNull String connect4PlaceholderItemDisplayName() {
-        return ChatColor.translateAlternateColorCodes('&', connect4_items_placeholder_displayName);
+    public @NotNull String placeholderItemDisplayName() {
+        return ChatColor.translateAlternateColorCodes('&', items_placeholder_displayName);
     }
 
     /**
@@ -175,17 +197,17 @@ public class SettingsManager {
      * ColorChat: '&'
      * @param connect4_items_placeholder_displayName parameter to set
      */
-    public void connect4PlaceholderItemDisplayName(@NotNull String connect4_items_placeholder_displayName) {
-        this.connect4_items_placeholder_displayName = connect4_items_placeholder_displayName;
-        this.config.set(path_connect4_items_placeholder_displayName, connect4_items_placeholder_displayName);
+    public void placeholderItemDisplayName(@NotNull String connect4_items_placeholder_displayName) {
+        this.items_placeholder_displayName = connect4_items_placeholder_displayName;
+        this.config.set(path_items_placeholder_displayName, connect4_items_placeholder_displayName);
         this.save();
     }
 
     /**
      * @return connect4_items_yourTurn_displayName
      */
-    public @NotNull String connect4YourTurnItemDisplayName() {
-        return ChatColor.translateAlternateColorCodes('&', connect4_items_yourTurn_displayName);
+    public @NotNull String yourTurnItemDisplayName() {
+        return ChatColor.translateAlternateColorCodes('&', items_yourTurn_displayName);
     }
 
     /**
@@ -193,17 +215,17 @@ public class SettingsManager {
      * ColorChat: '&'
      * @param connect4_items_yourTurn_displayName parameter to set
      */
-    public void connect4YourTurnItemDisplayName(@NotNull String connect4_items_yourTurn_displayName) {
-        this.connect4_items_yourTurn_displayName = connect4_items_yourTurn_displayName;
-        this.config.set(path_connect4_items_yourTurn_displayName , connect4_items_yourTurn_displayName);
+    public void yourTurnItemDisplayName(@NotNull String connect4_items_yourTurn_displayName) {
+        this.items_yourTurn_displayName = connect4_items_yourTurn_displayName;
+        this.config.set(path_items_yourTurn_displayName, connect4_items_yourTurn_displayName);
         this.save();
     }
 
     /**
      * @return connect4_items_opponentsTurn_displayName
      */
-    public @NotNull String connect4OpponentsTurnItemDisplayName() {
-        return ChatColor.translateAlternateColorCodes('&', connect4_items_opponentsTurn_displayName);
+    public @NotNull String opponentsTurnItemDisplayName() {
+        return ChatColor.translateAlternateColorCodes('&', items_opponentsTurn_displayName);
     }
 
     /**
@@ -211,17 +233,17 @@ public class SettingsManager {
      * ColorChat: '&'
      * @param connect4_items_opponentsTurn_displayName parameter to set
      */
-    public void connect4OpponentsTurnItemDisplayName(@NotNull String connect4_items_opponentsTurn_displayName) {
-        this.connect4_items_opponentsTurn_displayName = connect4_items_opponentsTurn_displayName;
-        this.config.set(path_connect4_items_opponentsTurn_displayName , connect4_items_opponentsTurn_displayName);
+    public void opponentsTurnItemDisplayName(@NotNull String connect4_items_opponentsTurn_displayName) {
+        this.items_opponentsTurn_displayName = connect4_items_opponentsTurn_displayName;
+        this.config.set(path_items_opponentsTurn_displayName, connect4_items_opponentsTurn_displayName);
         this.save();
     }
 
     /**
      * @return connect4_items_yourChipItem_displayName
      */
-    public @NotNull String connect4YourChipItemDisplayName() {
-        return ChatColor.translateAlternateColorCodes('&', connect4_items_yourChipItem_displayName);
+    public @NotNull String yourChipItemDisplayName() {
+        return ChatColor.translateAlternateColorCodes('&', items_yourChipItem_displayName);
     }
 
     /**
@@ -229,17 +251,17 @@ public class SettingsManager {
      * ColorChat: '&'
      * @param connect4_items_yourChipItem_displayName parameter to set
      */
-    public void connect4YourChipItemDisplayName(@NotNull String connect4_items_yourChipItem_displayName) {
-        this.connect4_items_yourChipItem_displayName = connect4_items_yourChipItem_displayName;
-        this.config.set(path_connect4_items_yourChipItem_displayName , connect4_items_yourChipItem_displayName);
+    public void yourChipItemDisplayName(@NotNull String connect4_items_yourChipItem_displayName) {
+        this.items_yourChipItem_displayName = connect4_items_yourChipItem_displayName;
+        this.config.set(path_items_yourChipItem_displayName, connect4_items_yourChipItem_displayName);
         this.save();
     }
 
     /**
      * @return connect4_items_opponentsChipItem_displayName
      */
-    public @NotNull String connect4OpponentsChipItemDisplayName() {
-        return ChatColor.translateAlternateColorCodes('&', connect4_items_opponentsChipItem_displayName);
+    public @NotNull String opponentsChipItemDisplayName() {
+        return ChatColor.translateAlternateColorCodes('&', items_opponentsChipItem_displayName);
     }
 
     /**
@@ -247,9 +269,9 @@ public class SettingsManager {
      * ColorChat: '&'
      * @param connect4_items_opponentsChipItem_displayName parameter to set
      */
-    public void connect4OpponentsChipItemDisplayName(@NotNull String connect4_items_opponentsChipItem_displayName) {
-        this.connect4_items_opponentsChipItem_displayName = connect4_items_opponentsChipItem_displayName;
-        this.config.set(path_connect4_items_opponentsChipItem_displayName , connect4_items_opponentsChipItem_displayName);
+    public void opponentsChipItemDisplayName(@NotNull String connect4_items_opponentsChipItem_displayName) {
+        this.items_opponentsChipItem_displayName = connect4_items_opponentsChipItem_displayName;
+        this.config.set(path_items_opponentsChipItem_displayName, connect4_items_opponentsChipItem_displayName);
         this.save();
     }
 }
